36c25ddbe3cecf14d0551da5e2925d89a1565f95
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2025-02-10 15:00:33 +0530

    Added logger

--- BasicWindowService/Program.cs
+++ BasicWindowService/Program.cs
@@ -1,7 +1,21 @@
+using Serilog;
 using BasicWindowService;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
+
+// Added Logger Configuration
+Log.Logger = 
+            new LoggerConfiguration().MinimumLevel.Debug()
+            .WriteTo.File("logs/log - .txt", rollingInterval: RollingInterval.Day).CreateLogger();
+
+// Added logger to services
+builder.Logging.Services.AddSerilog();
+
+// Adding application to Windows Services
+builder.Services.AddWindowsService(options => {
+    options.ServiceName = "WinServe by Gamedemons";
+});
 
 var host = builder.Build();
 host.Run();
--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Basic Window Service
 
 This is a basic Windows Background Service Application
+
+Register your service application to Services Control Manager
+
+```bash
+sc.exe create "[Write your service name here]" binpath="[Your Executable's Path]" 
+```
+
+Start your service
+
+```bash
+sc.exe start "[Your service name]"
+```
+
+> [!Note]
+> The service name you give in `builder.Services` options will show up in `Event Viewer`
